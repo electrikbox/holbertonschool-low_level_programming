5f5ad63e9b4b667ffbe7aef79275dffb82d44437
electrikbox <electrikbox@icloud.com>
2023-06-20 02:11:37 -0700

    replace 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Print '_putchar'
@@ -8,6 +8,13 @@
 
 int main(void)
 {
-	printf("%s\n", "_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
