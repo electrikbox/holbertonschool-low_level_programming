ef9354a0843ebfbbc42923a16b4095c61e5abfbe
electrikbox <electrikbox@icloud.com>
2023-06-20 06:06:14 -0700

    add task 6: print integer absolute

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - check the code.
+ *
+ * @n: parameter
+ *
+ * Return: -n if int is smaller than 0.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (n);
+}
