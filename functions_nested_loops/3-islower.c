48a27e4a998a2805a5166a8564fdfdc18f1598aa
electrikbox <electrikbox@icloud.com>
2023-06-20 05:04:11 -0700

    add task 3: check if char is lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - check the code.
+ *
+ * @c: function parameter
+ *
+ * Return: 1 if lower, 0 if not.
+ */
+
+int _islower(int c)
+{
+	if (c >= 97)
+	{
+		return (1);
+	}
+	return (0);
+}
