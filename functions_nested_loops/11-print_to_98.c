3e727c1c02027b459ac1c35a9b71775755ee18c5
electrikbox <electrikbox@icloud.com>
2023-06-20 13:48:09 -0700

    add task 11: print all natural numbers to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_to_98 - check the code.
+ *
+ * @n: int parameter
+ *
+ * Return: no return.
+ */
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n >= 0)
+		{
+			if (n > 99)
+			{
+				_putchar((n / 100) + '0');
+				_putchar((n / 10) % 10 + '0');
+			}
+			else if (n > 9)
+			{
+				_putchar(n / 10 + '0');
+			}
+			_putchar(n % 10 + '0');
+		}
+		else
+		{
+			_putchar('-');
+			if (n < -99)
+			{
+				_putchar((n / 100) * -1 + '0');
+				_putchar((n / 10) * -1 + '0');
+			}
+			else if (n < -9)
+			{
+				_putchar((n / 10) * -1 + '0');
+			}
+			_putchar((n % 10) * -1 + '0');
+		}
+		_putchar(',');
+		_putchar(' ');
+		n < 98 ? n++ : n--;
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+}
