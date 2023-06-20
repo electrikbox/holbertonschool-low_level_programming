08b0902fba203cdde6b124592989bad95cacd745
electrikbox <electrikbox@icloud.com>
2023-06-20 15:15:18 -0700

    add comment

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _isalpha - check the code.
+ *
+ * @c: int parameter
+ *
+ * Return: 0.
+ */
+
 int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
