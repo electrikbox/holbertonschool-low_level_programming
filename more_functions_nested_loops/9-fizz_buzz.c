63dd2499197eaece8872b610b0b38b87fb368a36
electrikbox <electrikbox@icloud.com>
2023-06-21 15:23:30 -0700

    try avoid stderr

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,21 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
+		{
 			printf("FizzBuzz ");
+		}
 		else if (num % 3 == 0)
+		{
 			printf("Fizz ");
+		}
 		else if (num % 5 == 0)
+		{
 			printf("Buzz ");
+		}
 		else
+		{
 			printf("%d ", num);
+		}
 	}
 	return (0);
 }
