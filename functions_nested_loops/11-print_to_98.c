7798e8ded82e8fa96486d94dfb67a66d982761d8
electrikbox <electrikbox@icloud.com>
2023-06-21 01:53:27 -0700

    refact code

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - check the code.
@@ -12,38 +13,8 @@ void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		if (n >= 0)
-		{
-			if (n > 99)
-			{
-				_putchar((n / 100) + '0');
-				_putchar((n / 10) % 10 + '0');
-			}
-			else if (n > 9)
-			{
-				_putchar(n / 10 + '0');
-			}
-			_putchar(n % 10 + '0');
-		}
-		else
-		{
-			_putchar('-');
-			if (n < -99)
-			{
-				_putchar((n / 100) * -1 + '0');
-				_putchar((n / 10) * -1 % 10 + '0');
-			}
-			else if (n < -9)
-			{
-				_putchar((n / 10) * -1 + '0');
-			}
-			_putchar((n % 10) * -1 + '0');
-		}
-		_putchar(',');
-		_putchar(' ');
+		printf("%d, ", n);
 		n < 98 ? n++ : n--;
 	}
-	_putchar('9');
-	_putchar('8');
-	_putchar('\n');
+	printf("98\n");
 }
