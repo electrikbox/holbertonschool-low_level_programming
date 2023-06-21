43763d607c1f6c5ece6f1d65843cdcb7d7ba9006
electrikbox <electrikbox@icloud.com>
2023-06-21 13:01:20 -0700

    add task 2: multiplicate a and b int

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplication of a and b int.
+ *
+ * @a: int 1.
+ *
+ * @b: int 2.
+ *
+ * Return: no return.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
