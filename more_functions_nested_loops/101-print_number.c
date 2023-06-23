180f6fab4712e9edcd2faf1d406476cb9fcd649c
electrikbox <electrikbox@icloud.com>
2023-06-23 09:21:22 -0700

    add task 101 print int with putchar

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n > 999)
+	{
+		_putchar(n / 1000 + '0');
+	}
+	if (n > 99)
+	{
+		_putchar((n / 100) % 10 + '0');
+	}
+	if (n > 9)
+	{
+		_putchar((n / 10) % 10 + '0');
+	}
+	_putchar(n % 10 + '0');
+	_putchar('\n');
+}
