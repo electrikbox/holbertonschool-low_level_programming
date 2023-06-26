861d9f3b90a48b35e6bc532c8018c441b1d8b8f2
electrikbox <electrikbox@icloud.com>
2023-06-26 13:22:52 -0700

    change int to unsigned int for result var

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,10 +12,9 @@
 
 int _atoi(char *s)
 {
-	int result = 0;
+	unsigned int result = 0;
 	int sign = 1;
 	int i = 0;
-	int overflow = 0;
 
 	while (s[i] && !(s[i] >= '0' && s[i] <= '9'))
 	{
@@ -26,18 +25,9 @@ int _atoi(char *s)
 
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		if ((result < INT_MIN / 10) ||
-				(result == INT_MIN / 10 && (s[i] - '0') > -(INT_MIN % 10)))
-		{
-			overflow = 1;
-			break;
-		}
 		result = result * 10 + (s[i] - '0');
 		i++;
 	}
 
-	if (overflow)
-		return (sign * INT_MAX);
-	else
-		return (sign * result);
+	return (sign * result);
 }
