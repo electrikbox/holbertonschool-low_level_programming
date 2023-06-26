45e052831feddba35a5e139dbe716f88b9951d4b
electrikbox <electrikbox@icloud.com>
2023-06-26 09:40:08 -0700

    add task 8: print all elements from array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print all elements from array
+ *
+ * @a: array
+ *
+ * @n: number of elements
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	n = sizeof(*a);
+
+	while (i < n + 1)
+	{
+		printf("%d\n", a[i]);
+		i++;
+	}
+}
