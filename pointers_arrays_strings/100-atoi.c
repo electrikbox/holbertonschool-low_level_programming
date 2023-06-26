252c6b533223f2a2514a09a885086704848f8357
electrikbox <electrikbox@icloud.com>
2023-06-26 12:36:50 -0700

    fix wronf var names

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -15,16 +15,16 @@ int _atoi(char *s)
 	int sign = 1;
 	int i = 0;
 
-	while (str[i] && !(str[i] >= '0' && str[i] <= '9'))
+	while (s[i] && !(s[i] >= '0' && s[i] <= '9'))
 	{
-		if (str[i] == '-')
+		if (s[i] == '-')
 			sign *= -1;
 		i++;
 	}
 
-	while (str[i] >= '0' && str[i] <= '9')
+	while (s[i] >= '0' && s[i] <= '9')
 	{
-		result = result * 10 + (str[i] - '0');
+		result = result * 10 + (s[i] - '0');
 		i++;
 	}
 
