f336ed6f4e2430b094034cfd75bfc72964812532
electrikbox <electrikbox@icloud.com>
2023-06-28 10:28:48 -0700

    add task 2: search char

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - move pointer start
+ *
+ * @s: given string
+ * @c: char to find
+ *
+ * Return: new position or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			return (s += i);
+		}
+		i++;
+	}
+	return ('\0');
+}
