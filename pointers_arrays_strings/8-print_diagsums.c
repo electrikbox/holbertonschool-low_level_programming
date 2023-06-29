61abb17b45f64f95c2f3ea60cc3a9be48438ac46
electrikbox <electrikbox@icloud.com>
2023-06-29 07:55:41 -0700

    fix missing ;

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -16,7 +16,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum1 += a[i * size + i];
-		sum2 += a[(i + 1) * size - (i + 1)]
+		sum2 += a[(i + 1) * size - (i + 1)];
 	}
 
 	printf("%d, %d\n", sum1, sum2);
