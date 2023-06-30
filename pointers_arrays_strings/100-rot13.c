e4d0e43ed4cc5318b5a61f615215a105cf8cfd4b
electrikbox <electrikbox@icloud.com>
2023-06-30 04:19:03 -0700

    add task 100: encrypt with rot13

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - encrypte or decrypt sentence in rot13
+ * @str: given string
+ * Return: return new string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = rot13[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
