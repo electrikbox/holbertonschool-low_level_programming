1a1dd9b6d3c7e68d63faf7b97c2a9ab25b338e05
electrikbox <electrikbox@icloud.com>
2023-07-07 04:53:56 -0700

    new try

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -8,22 +8,41 @@
 int main(void)
 {
 	int i;
-	unsigned long num1, num2, sum;
-
+	unsigned long num1, num2, sum, sumFront, sumBack;
+	unsigned long frontNum, backNum;
+	
 	num1 = 1;
 	num2 = 2;
 
-	for (i = 1; i <= 98; i++)
-	{
-		printf("%lu", num1);
+	printf("%lu, %lu, ", num1, num2);
 
+	for (i = 3; i <= 98; i++)
+	{
+		if (num1 + num2 > 10000000000)
+		{
+			frontNum = (num1 + num2) / 10000000000;
+			backNum = (num1 + num2) % 10000000000;
+
+			printf("%lu%lu", frontNum, backNum);
+
+			sumBack = num1 + num2;
+			num1 = num2;
+			num2 = sumBack;
+		}
+		else
+		{
+			/*printf("%lu", sum);*/
+
+			sum = num1 + num2;
+                        num1 = num2;
+                        num2 = sum;
+
+			printf("%lu", sum);
+		}
 		if (i != 98)
 		{
 			printf(", ");
 		}
-		sum = num1 + num2;
-		num1 = num2;
-		num2 = sum;
 	}
 	putchar('\n');
 	return (0);
