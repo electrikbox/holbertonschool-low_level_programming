ad810ed5a3e5f34167f9853db0d38219c6c2ecde
electrikbox <electrikbox@icloud.com>
2023-07-10 04:46:48 -0700

    add _strlen and _strcpy

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,6 +3,44 @@
 #include <string.h>
 
 /**
+ * _strcpy - copy string from src to dest
+ * @dest: destination
+ * @src: source
+ * Return: return destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strlen - give the array lenght
+ * @s: function parameter
+ * Return: return the lenght
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+
+/**
  * _strdup - duplicate string
  * @str: given string
  * Return: duplicate string
@@ -15,12 +53,12 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	duplicate = malloc(strlen(str) + 1);
+	duplicate = malloc(_strlen(str) + 1);
 
 	if (duplicate == NULL)
 		return (NULL);
 
-	strcpy(duplicate, str);
+	_strcpy(duplicate, str);
 
 	return (duplicate);
 }
