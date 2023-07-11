9d560d2d1bf133d3b8222cae5b1c92c6ac90f7b8
electrikbox <electrikbox@icloud.com>
2023-07-11 02:20:51 -0700

    add if null

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -45,6 +45,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (newArray == NULL)
 		return (NULL);
 
+	if (newArray == NULL)
+		return (NULL);
+
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		newArray[i] = s1[i];
