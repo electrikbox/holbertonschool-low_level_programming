e5e3fecd1e14a7acb5eb6e8a8353a6dae5a0a9a2
electrikbox <electrikbox@icloud.com>
2023-07-12 14:33:54 +0200

    update for betty

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -10,34 +10,34 @@
  */
 char *argstostr(int ac, char **av)
 {
-    char *newArray;
-    int i, j, count = 0;
+	char *newArray;
+	int i, j, count = 0;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-    for (i = 0; i < ac; i++)
-        for (j = 0; av[i][j] != '\0'; j++)
-            count += 1;
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j] != '\0'; j++)
+			count += 1;
 
-    newArray = malloc(sizeof(char) * (count + ac + 1));
+	newArray = malloc(sizeof(char) * (count + ac + 1));
 
-    if (newArray == NULL)
-        return (NULL);
+	if (newArray == NULL)
+		return (NULL);
 
-    count = 0;
+	count = 0;
 
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-        {
-            newArray[count] = av[i][j];
-            count++;
-        }
-        newArray[count] = '\n';
-        count++;
-    }
-    newArray[count] = '\0';
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			newArray[count] = av[i][j];
+			count++;
+		}
+		newArray[count] = '\n';
+		count++;
+	}
+	newArray[count] = '\0';
 
-    return (newArray);
+	return (newArray);
 }
