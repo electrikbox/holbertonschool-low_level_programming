56d18861938044659a70fbf0c09ed7eaefa069cf
electrikbox <electrikbox@icloud.com>
2023-07-13 15:15:25 +0200

    fix for betty

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -15,31 +15,31 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
 
-    if (ptr == NULL)
-    {
-        ptr = malloc(new_size);
-        return (ptr);
-    }
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
 
-    if (new_size == 0)
-    {
-        free(ptr);
-        return (NULL);
-    }
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
-    if (new_size <= old_size)
-    {
-        return (ptr);
-    }
+	if (new_size <= old_size)
+	{
+		return (ptr);
+	}
 
-    new_ptr = malloc(new_size);
+	new_ptr = malloc(new_size);
 
 	if (new_ptr == NULL)
-        return (NULL);
+		return (NULL);
 
-    memcpy(new_ptr, ptr, old_size);
+	memcpy(new_ptr, ptr, old_size);
 
-    free(ptr);
+	free(ptr);
 
-    return (new_ptr);
+	return (new_ptr);
 }
