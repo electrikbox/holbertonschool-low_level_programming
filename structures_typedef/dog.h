94ed893da9414ee9ae79d4355baf1013c77b61c4
electrikbox <electrikbox@icloud.com>
2023-07-17 09:49:00 +0200

    add task 0: create a typedef structure called dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - structure ti init dog information (owner, age, name)
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog owner
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+
+#endif
