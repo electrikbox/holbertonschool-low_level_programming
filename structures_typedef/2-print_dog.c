99bf849beaf6a2e9a1acf0f51a21011294f39469
electrikbox <electrikbox@icloud.com>
2023-07-17 10:24:38 +0200

    add task 2: print structure

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print all dog structure infos in STDOUT
+ * @d: given dog structure
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf("nil\n");
+	else
+	{
+		printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+
+		if (d->age == 0.0f)
+			printf("Age: (nil)\n");
+
+		else
+			printf("Age: %f\n", d->age);
+
+		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
+	}
+}
