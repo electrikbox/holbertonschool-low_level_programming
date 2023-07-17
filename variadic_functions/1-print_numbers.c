e884f0cfaf48c2f7642c66cbb96897166e7e2e91
electrikbox <electrikbox@icloud.com>
2023-07-17 19:18:26 +0200

    add task 1: prints numbers, followed by a new line

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print all args with separator
+ * @separator: given separator
+ * @n: args len
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (separator == NULL)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
