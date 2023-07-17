1dc3097651c4cebc79ad15ac237d29cc13241055
electrikbox <electrikbox@icloud.com>
2023-07-17 10:27:13 +0200

    update print nil for return in first condition

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,17 +8,15 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		printf("nil\n");
+		return;
+
+	printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+
+	if (d->age < 0.0f)
+		printf("Age: (nil)\n");
+
 	else
-	{
-		printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+		printf("Age: %f\n", d->age);
 
-		if (d->age == 0.0f)
-			printf("Age: (nil)\n");
-
-		else
-			printf("Age: %f\n", d->age);
-
-		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
-	}
+	printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 }
