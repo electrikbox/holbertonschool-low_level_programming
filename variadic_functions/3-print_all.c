ae9bc0ccaaf106c35cf6bcfb05f94f2e1dcf1899
electrikbox <electrikbox@icloud.com>
2023-07-18 10:00:22 +0200

    rework all code to use typedef and pointer function

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,45 +1,83 @@
 #include "variadic_functions.h"
 
 /**
+ * print_char - printf if arg is a char
+ * @args: given arg
+*/
+	void print_char(va_list args)
+	{
+		printf("%c", va_arg(args, int));
+	}
+
+/**
+ * print_int - printf if arg is a int
+ * @args: given arg
+*/
+	void print_int(va_list args)
+	{
+		printf("%d", va_arg(args, int));
+	}
+
+/**
+ * print_float - printf if arg is a float
+ * @args: given arg
+*/
+	void print_float(va_list args)
+	{
+		printf("%f", va_arg(args, double));
+	}
+
+/**
+ * print_string - printf if arg is a char *
+ * @args: given arg
+*/
+	void print_string(va_list args)
+	{
+		char *str = va_arg(args, char *);
+
+		if (str == NULL)
+		{
+			printf("(nil)");
+			return;
+		}
+		printf("%s", str);
+	}
+
+/**
  * print_all - print all arg with given type
  * @format: list of valid format to print
 */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int index = 0;
-	const char *separator = ", ";
-	char *str;
+	unsigned int index1 = 0, index2 = 0;
+	const char *separator = "";
+
+	charTypes types[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
 
 	va_start(args, format);
 
-	while (format && format[index] != '\0')
+	while (format != NULL && format[index1] != '\0')
 	{
-		switch (format[index])
-		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char*);
-				if (str == NULL)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", str);
-				break;
-		}
-		if (format[index + 1])
-			printf("%s", separator);
+		index2 = 0;
 
-		index++;
+		while (types[index2].argType != '\0')
+		{
+			if (format[index1] == types[index2].argType)
+			{
+				printf("%s", separator);
+				types[index2].func(args);
+				separator = ", ";
+			}
+			index2++;
+		}
+		index1++;
 	}
 	printf("\n");
 	va_end(args);
