a8ed5d0ccdd074dd1269eb0a7c0e1f5d1333d356
electrikbox <electrikbox@icloud.com>
2023-07-31 17:54:51 +0200

    add task 7: insert new node at index

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert new node at index
+ * @h: given linked list
+ * @idx: index when new node should be added
+ * @n: data for new node
+ * Return: new node
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newNode, *temp;
+	unsigned int count = 0;
+
+	if (h == NULL)
+		return (NULL);
+
+	temp = *h;
+
+	while (temp != NULL)
+	{
+		if (count == idx - 1)
+		{
+			newNode = malloc(sizeof(dlistint_t));
+
+			if (newNode == NULL)
+				return (NULL);
+
+			newNode->n = n;
+			newNode->prev = temp;
+			newNode->next = temp->next;
+			temp->next = newNode;
+
+			if (newNode->next != NULL)
+				newNode->next->prev = newNode;
+
+			return (newNode);
+		}
+
+		count++;
+		temp = temp->next;
+	}
+
+	return (NULL);
+}
