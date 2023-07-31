7f140adac015595038b19bc77b7296f79e45b703
electrikbox <electrikbox@icloud.com>
2023-07-31 13:30:35 +0200

    add task 0: print data from linked list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print data from linked list
+ * @h: given linked list
+ * Return: linked list length
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t hLen = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		hLen++;
+		h = h->next;
+	}
+
+	return (hLen);
+}
