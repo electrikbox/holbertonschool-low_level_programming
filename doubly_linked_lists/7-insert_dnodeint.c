1b2f3986aa68ee53e3ae09e0b2e73910cfae7e3e
electrikbox <electrikbox@icloud.com>
2023-07-31 19:04:59 +0200

    optimizing code

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,38 +9,35 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newNode, *temp;
+	dlistint_t *newNode, *temp = *h;
 	unsigned int count = 0;
 
-	if (*h == NULL || idx == 0)
-	{
-		newNode = add_dnodeint(h, n);
-		return (newNode);
-	}
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL || h == NULL)
+		return (NULL);
+
+	newNode->n = n;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
 	temp = *h;
 
-	while (temp != NULL)
+	while (count < idx - 1)
 	{
-		if (count == idx - 1)
-		{
-			newNode = malloc(sizeof(dlistint_t));
-			if (newNode == NULL)
-				return (NULL);
-
-			newNode->n = n;
-			newNode->prev = temp;
-			newNode->next = temp->next;
-			temp->next = newNode;
-
-			if (newNode->next != NULL)
-				newNode->next->prev = newNode;
-
-			return (newNode);
-		}
-		count++;
 		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+		count++;
 	}
 
-	return (NULL);
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	newNode->prev = temp;
+	newNode->next = temp->next;
+	temp->next->prev = newNode;
+	temp->next = newNode;
+
+	return (newNode);
 }
