5f5331bf2b1461879f5d1c4e0216ba28e38f407e
electrikbox <electrikbox@icloud.com>
2023-07-31 14:11:49 +0200

    update print_list code for backward print

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -21,3 +21,29 @@ size_t print_dlistint(const dlistint_t *h)
 
 	return (hLen);
 }
+
+/**
+ * _print_dlistint_backward - print data from linked list
+ * @h: given linked list
+ * Return: linked list length
+*/
+size_t _print_dlistint_backward(const dlistint_t *h)
+{
+	size_t hLen = 0;
+	const dlistint_t *temp = h;
+
+	if (h == NULL)
+		return (0);
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		hLen++;
+		temp = temp->prev;
+	}
+
+	return (hLen);
+}
--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,6 +17,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newNode->prev = NULL;
 	newNode->next = *head;
 
+	if (*head != NULL)
+		(*head)->prev = newNode;
+
 	*head = newNode;
 
 	return (newNode);
