25215f458d7bf2a6ad867f51bf5ecc0ca84792dc
electrikbox <electrikbox@icloud.com>
2023-07-31 15:15:31 +0200

    add task 5: get the node at given index

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get node at given index
+ * @head: given linked list
+ * @index: given index
+ * Return: node at gven index
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	dlistint_t *nodeAtIndex;
+
+	if (head == NULL)
+		return (NULL);
+
+	nodeAtIndex = head;
+
+	while (nodeAtIndex->next != NULL)
+	{
+		if (count == index)
+			return (nodeAtIndex);
+
+		count++;
+		nodeAtIndex = nodeAtIndex->next;
+	}
+
+	return (NULL);
+}
