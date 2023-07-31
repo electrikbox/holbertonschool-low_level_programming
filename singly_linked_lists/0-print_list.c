8634b094dfc68f9dc0135b499480c51303231763
electrikbox <electrikbox@icloud.com>
2023-07-31 10:30:23 +0200

    update while loop to print 0 and (nil) if no string in node

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,14 +10,16 @@ size_t print_list(const list_t *h)
 	size_t listLen = 0;
 
 	if (h == NULL)
-	{
 		return (0);
-	}
 
 	while (h != NULL)
 	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
 		listLen++;
-		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
 
