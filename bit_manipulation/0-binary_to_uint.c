a267af0d68d07602ca35a973f5f633e01dc12ab3
electrikbox <electrikbox@icloud.com>
2023-08-01 14:10:45 +0200

    add null check

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,6 +9,9 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0, index = 0;
 
+	if (b == NULL)
+		return (0);
+
 	while (b[index] != '\0')
 	{
 		if (b[index] != '0' && b[index] != '1')
