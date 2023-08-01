0bf33884a504e3e3eb0641aa0f1eae9e3968d412
electrikbox <electrikbox@icloud.com>
2023-08-01 14:08:37 +0200

    fix for betty

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * binary_to_uint - transform given binary to int
+ * @b: given binary
+ * Return: 0 if not a binary, int if it is
+*/
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0, index = 0;
