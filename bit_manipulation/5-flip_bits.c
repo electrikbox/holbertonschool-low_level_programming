7eb537cde81799edabf36a5746622a933fdba5e3
electrikbox <electrikbox@icloud.com>
2023-08-02 13:27:02 +0200

    add task 5: count flip needed between 2 numbers

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - count the flip count needed to have the same number
+ * @n: first number
+ * @m: second number
+ * Return: count of flip needed
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int flipCount = 0, count = 0, size = sizeof(n) * 8 - 1;
+
+	while (count < size)
+	{
+		if ((n & 1) != (m & 1))
+			flipCount++;
+		n >>= 1;
+		m >>= 1;
+		count++;
+	}
+	return (flipCount);
+}
