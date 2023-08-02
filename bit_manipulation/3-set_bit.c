9d7515e22a2a71116e2f9aedf4325ae9cf8d6c02
electrikbox <electrikbox@icloud.com>
2023-08-02 10:40:55 +0200

    add task 3: set a bit at index

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_bit - set bit to 1 at index position
+ * @n: given number pointer
+ * @index: index where bit should be set
+ * Return: -1 if failes, 1 otherwise
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask, size = sizeof(n) * 8 - 1;
+
+	if (index > size)
+		return (-1);
+
+	mask = 1 << index;
+	*n = *n | mask;
+
+	return (1);
+}
