ab5356f28beba36e2d25cb9fc0fb470456c17dd6
electrikbox <electrikbox@icloud.com>
2023-08-02 13:39:54 +0200

    fix for betty

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,6 +1,10 @@
 #include "lists.h"
 
-void startup (void)
+/**
+ * startup - print a sentence before main
+*/
+void startup(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
