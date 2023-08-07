06329bc491fe80dbf4daea0a71e68e65da11041b
electrikbox <electrikbox@icloud.com>
2023-08-07 20:33:26 +0200

    re add the while loop since only 1024 char are copied without

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -23,40 +23,37 @@ void printError(const char *message, const char *spec, int errorCode)
 */
 int main(int argc, char **argv)
 {
-    int file, copy;
-    char buffer[1024];
-    ssize_t readFile, writeFile;
+	int file, readFile, copy;
+	char *buffer[1024];
 
-    if (argc != 3)
-        printError("Usage: cp file_from file_to", NULL, 97);
+	if (argc != 3)
+		printError("Usage: cp file_from file_to", NULL, 97);
 
-    file = open(argv[1], O_RDONLY);
-    if (file < 0)
-        printError("Error: Can't read from file", argv[1], 98);
+	file = open(argv[1], O_RDONLY);
+	if (file < 0)
+		printError("Error: Can't read from file", argv[1], 98);
 
-    copy = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (copy < 0)
-        printError("Error: Can't write to", argv[2], 99);
+	copy = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
-    readFile = read(file, buffer, sizeof(buffer));
-    if (readFile < 0)
-        printError("Error: Can't read from file", argv[1], 98);
+	while ((readFile = read(file, buffer, 1024)) > 0)
+	{
+		if (write(copy, buffer, readFile) != readFile || copy < 0)
+			printError("Error: Can't write to", argv[2], 99);
+	}
 
-    writeFile = write(copy, buffer, readFile);
-    if (writeFile != readFile || writeFile < 0)
-        printError("Error: Can't write to", argv[2], 99);
+	if (readFile < 0)
+		printError("Error: Can't read from file", argv[1], 98);
 
-    if (close(file) < 0)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
-        exit(100);
-    }
+	if (close(file) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
+		exit(100);
+	}
 
-    if (close(copy) < 0)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", copy);
-        exit(100);
-    }
-
-    return (0);
+	if (close(copy) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", copy);
+		exit(100);
+	}
+	return (0);
 }
