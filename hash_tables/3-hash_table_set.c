66b70397c9a98647db202c94d0777e436af127e7
electrikbox <electrikbox@icloud.com>
2023-08-08 16:06:46 +0200

    completely redo the 'if exist key' condition

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,18 +10,35 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *newNode;
+	hash_node_t *newNode, *currentNode;
 
 	if (!ht || !key || key[0] == '\0' || !value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	currentNode = ht->array[index];
+
+	/* check if key already exist */
+	while (currentNode != NULL)
+	{
+		if (strcmp(currentNode->key, key) == 0)
+		{
+			free(currentNode->value);
+			currentNode->value = strdup(value);
+			return (1);
+		}
+		currentNode = currentNode->next;
+	}
+
+	/* key doesn't already exist */
 	newNode = malloc(sizeof(hash_node_t));
 	if (!newNode)
 		return (0);
 
-	newNode->value = strdup(value);
 	newNode->key = strdup(key);
+	newNode->value = strdup(value);
+	newNode->next = ht->array[index];
+	ht->array[index] = newNode;
 
 	if (!newNode->value || !newNode->key)
 	{
@@ -30,8 +47,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(newNode);
 		return (0);
 	}
-
-	ht->array[index] = newNode;
-
 	return (1);
 }
