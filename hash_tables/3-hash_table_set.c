966f90a852cef590a628caae2af0a9ab8c16d13e
electrikbox <electrikbox@icloud.com>
2023-08-08 15:18:19 +0200

    add task 3: set a new non null node in table

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - set a new non null node in table
+ * @ht: given table
+ * @key: given key
+ * @value: given value
+ * Return: 1 on success, 0 on failure
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *newNode;
+
+	if (!ht || !key || key[0] == '\0' || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	newNode = malloc(sizeof(hash_node_t));
+	if (!newNode)
+		return (0);
+
+	newNode->value = strdup(value);
+	newNode->key = strdup(key);
+
+	if (!newNode->value || !newNode->key)
+	{
+		free(newNode->value);
+		free(newNode->key);
+		free(newNode);
+		return (0);
+	}
+
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = newNode;
+	}
+	else
+	{
+		newNode->next = ht->array[index];
+		ht->array[index] = newNode;
+	}
+
+	return (1);
+}
